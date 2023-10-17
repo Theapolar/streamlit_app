81b0d30ccc1359dc5373a265dbbcbd892eac19f3
Thea Waalen Borch <thea@Theas-MacBook-Pro-2.local>
2023-10-17 13:53:45 +0200

    First commit

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import openai
+
+def chatbot_response(skills_list):
+    openai.api_key = "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"
+    model = "gpt-4"
+    max_tokens = 900
+
+    prompt = f"The user has skills in or cares about {skills_list}...Generate a list of 10 highly creative and unique business ideas for solopreneurs incorporating best practices from various frameworks like Russel Brunson's 'value ladder,' Lean Startup's 'MVP' (Minimum Viable Product), and Value Proposition Design's 'customer job' concept. The ideas should align with their skills and interests, be actionable, require minimal capital, and offer quick returns. Keep the descriptions under one sentence. Also, list 5 concise skills or subjects they should focus on learning to excel in these services. make sure to fit 10 ideas an 5 skills to learn within max 900 tokens.No waste of words"
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=max_tokens
+    )
+    
+    message = response['choices'][0]['message']['content']
+    return message.strip()
+
+st.title("GPT-4 Chatbot for Business Ideas")
+
+user_input = st.text_input("Enter your skills or interests:")
+if st.button("Send"):
+    if user_input.lower() == "bye":
+        st.write("Goodbye! Have a great day!")
+    else:
+        response = chatbot_response(user_input)
+        st.write("Chatbot:", response)
