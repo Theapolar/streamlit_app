30bd33e9d1707d7fc085f7c5a7de63eeb464c3f4
Thea Waalen Borch <thea@Theas-MacBook-Pro-2.local>
2023-10-20 11:20:12 +0200

    Your commit message here

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,15 @@
 import os
 import streamlit as st
-st.set_page_config(layout="wide", page_icon=None, initial_sidebar_state="auto", page_title=None, menu_items={'Get help': None, 'Report a bug': None, 'About': None})
-
 import openai
+
+st.set_page_config(layout="wide", page_icon=None, initial_sidebar_state="auto", page_title=None, menu_items={'Get help': None, 'Report a bug': None, 'About': None})
 
 def chatbot_response(skills_list):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     model = "gpt-4"
     max_tokens = 900
-
-
-    prompt = f"The user has skills in or cares about {skills_list}...Generate a list of 10 highly creative and unique business ideas for solopreneurs incorporating best practices from various frameworks like Russel Brunson's 'value ladder,' Lean Startup's 'MVP' (Minimum Viable Product), and Value Proposition Design's 'customer job' concept. The ideas should align with their skills and interests, be actionable, require minimal capital, and offer quick returns. Keep the descriptions under one sentence. Also, list 5 concise skills or subjects they should focus on learning to excel in these services. make sure to fit 10 ideas an 5 skills to learn within max 900 tokens.No waste of words"
+    prompt = f"The user has skills in or cares about {skills_list}..."
+    # rest of your code
 
     response = openai.ChatCompletion.create(
         model=model,
@@ -20,11 +19,11 @@ def chatbot_response(skills_list):
         ],
         max_tokens=max_tokens
     )
-    
+
     message = response['choices'][0]['message']['content']
     return message.strip()
 
-st.title("Aideas to monitize")
+st.title("Ideas to Monetize")
 
 user_input = st.text_input("Enter your skills, interests, curiosities")
 if st.button("Send"):
