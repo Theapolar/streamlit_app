59c0cc0e1b5c1828794757864353b9a76263941b
Thea Waalen Borch <thea@Theas-MacBook-Pro-2.local>
2023-10-20 11:10:58 +0200

    Fixed indentation issue in chatbot_response function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ st.set_page_config(layout="wide", page_icon=None, initial_sidebar_state="auto", 
 import openai
 
 def chatbot_response(skills_list):
-   openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     model = "gpt-4"
     max_tokens = 900
+
 
     prompt = f"The user has skills in or cares about {skills_list}...Generate a list of 10 highly creative and unique business ideas for solopreneurs incorporating best practices from various frameworks like Russel Brunson's 'value ladder,' Lean Startup's 'MVP' (Minimum Viable Product), and Value Proposition Design's 'customer job' concept. The ideas should align with their skills and interests, be actionable, require minimal capital, and offer quick returns. Keep the descriptions under one sentence. Also, list 5 concise skills or subjects they should focus on learning to excel in these services. make sure to fit 10 ideas an 5 skills to learn within max 900 tokens.No waste of words"
 
